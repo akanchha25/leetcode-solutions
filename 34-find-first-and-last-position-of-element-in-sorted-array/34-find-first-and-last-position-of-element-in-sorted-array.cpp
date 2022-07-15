dc6b3d26de065dbd86fb0b3422976ecb3742ef38
akanchha25 <75972629+akanchha25@users.noreply.github.com>
2022-07-16 01:30:59 +0530

    Time: 4 ms (96.45%), Space: 13.6 MB (63.15%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,51 +1,49 @@
 class Solution {
 public:
+    
+    
+      
+       int find_helper(vector<int>& nums, int target,bool isFirst) 
+           
+       {
+            int l=0,h=(nums.size()-1);
+        int ans=-1;
+        while(l<=h)
+        {
+            int mid=l+((h-l)/2);
+            if(target<nums[mid])
+            {
+                h=mid-1;
+            }
+            else if(target>nums[mid])
+            {
+                l=mid+1;
+            }
+            else 
+            { 
+                ans=mid;
+                if(isFirst)
+                h=mid-1;
+                else 
+                    l=mid+1;
+            }
+        }
+        return ans;
+        
+       }
+    
+    
     vector<int> searchRange(vector<int>& nums, int target) {
         
-        int l=0,h=(nums.size()-1);
-        int first=-1,last=-1;
+       int left=find_helper( nums, target,1) ;
+        int right=find_helper( nums, target,0);
+        if(left<=right)
+            return{left,right};
+        else
+            return{-1,-1};
+      
         
-        //for first
-        while(l<=h)
-        {
-            int mid=l+((h-l)/2);
-            if(target<nums[mid])
-            {
-                h=mid-1;
-            }
-            else if(target>nums[mid])
-            {
-                l=mid+1;
-            }
-            else 
-            {
-                first=mid;
-                h=mid-1;
-            }
-        }
-        
-        //last
-          l=0;h=(nums.size()-1);
-        
-        while(l<=h)
-        {
-            int mid=l+((h-l)/2);
-            if(target<nums[mid])
-            {
-                h=mid-1;
-            }
-            else if(target>nums[mid])
-            {
-                l=mid+1;
-            }
-            else 
-            {
-                last=mid;
-                l=mid+1;
-            }
-        }
-        
-        return {first,last};
+
         
         
             
@@ -54,3 +52,6 @@ public:
         
     }
 };
+
+
+//code using find helper function
