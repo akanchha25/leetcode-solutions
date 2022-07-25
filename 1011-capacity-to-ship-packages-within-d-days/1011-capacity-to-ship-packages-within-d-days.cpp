8e25f4227ad1db44d482ab9f70c8395997d7b31e
akanchha25 <75972629+akanchha25@users.noreply.github.com>
2022-07-25 14:47:42 +0530

    Time: 118 ms (20.08%), Space: 26.2 MB (26.55%) - LeetHub

---
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+   bool isAllocation_possible(vector<int>& weights, int days,int max_weight)
+    {
+        int day=1,current_weight=0;
+        for(int i=0;i<weights.size();i++)
+        {
+            if(current_weight + weights[i]>max_weight)
+            {
+                day++;
+                if(day>days)
+                    return false;
+                current_weight=weights[i];
+            }
+            else{
+                current_weight+=weights[i];
+            }
+        }
+       return true;
+    }
+    
+    
+    
+    int shipWithinDays(vector<int>& weights, int days) {
+        int res=INT_MIN;
+        if(days>weights.size())
+            return -1;
+        
+        int l=*max_element(weights.begin(), weights.end());   
+        int h=accumulate(weights.begin(), weights.end(), 0);
+        
+        while(l<=h)
+        {
+            int mid=l+(h-l)/2;
+            if(isAllocation_possible(weights,days,mid))
+            {
+                res=mid;
+                h=mid-1;
+            }
+            else
+            {
+                l=mid+1;
+            }
+        }
+        return res;
+        
+    }
+};
