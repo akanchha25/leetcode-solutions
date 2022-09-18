c4138d054e77def5da804eda4d7724d11ec3bf25
akanchha25 <75972629+akanchha25@users.noreply.github.com>
2022-09-18 14:07:45 +0530

    Time: 758 ms (17.06%), Space: 134.8 MB (61.56%) - LeetHub

---
+++ 239-sliding-window-maximum/239-sliding-window-maximum.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        
+        deque<int> dq;
+        vector<int> ans;
+        
+        
+        //for finding maximum element in first window
+        for(int i=0;i<k;i++){
+            
+            while(!dq.empty()&&nums[i]>=nums[dq.back()]){
+                dq.pop_back();
+            }
+            dq.push_back(i);
+        }
+        
+        for(int i=k;i<nums.size();i++){
+            ans.push_back(nums[dq.front()]);
+            
+            while(!dq.empty()&&dq.front()==i-k){
+                dq.pop_front();
+            }
+            while(!dq.empty()&& nums[i]>nums[dq.back()]){
+                dq.pop_back();
+            }
+            dq.push_back(i);
+            
+        }
+        ans.push_back(nums[dq.front()]);
+        return ans;
+        
+    }
+};
